6c9cd63a347b8d17445262ce236517bfad2e7fde
Martin  Rouanet <martin.rouanet@epfedu.fr>
2024-11-20 12:48:46 +0100

    Création de la classe Pion représentant l’unité de base d’une combinaison, correspondant à chaque élément coloré. Dans cette classe chaque pion contient des informations sur sa couleur, accessibles via des accesseurs, ici 'R' pour rouge, 'B' pour bleu et 'V' pour vert. Ces couleurs sont retournées grâce à 'getCouleur()', et les lettres spécifiques à chaque couleurs sont retournées grâce à 'toString()'. Les pions sont combinés pour former une proposition ou une combinaison secrète. Création du main pour Pion. Ce programme appelle les différents objets Pion, qui sont 'R', 'B' et 'V'. Puis il lance un test des méthodes utilisés dans la class Pion, qui sont 'getCouleur()' et 'toString()'.

--- CPO_miniProjet/src/cpo_miniprojet/CPO_miniProjet.java
+++ CPO_miniProjet/src/cpo_miniprojet/CPO_miniProjet.java
@@ -15,7 +15,16 @@ public class CPO_miniProjet {
     /**
      * @param args the command line arguments
      */
+
     public static void main(String[] args) {
-        // TODO code application logic here
-}
+        // Instanciation de différents objets Pion
+        Pion pionRouge = new Pion('R'); // Rouge
+        Pion pionBleu = new Pion('B');  // Bleu
+        Pion pionVert = new Pion('V');  // Vert
+
+        // Test des méthodes getCouleur() et toString()
+        System.out.println("Representation du pion rouge : " + pionRouge.getCouleur());
+        System.out.println("Representation du pion bleu : " + pionBleu);
+        System.out.println("Representation du pion vert : " + pionVert.getCouleur());
+    }
 }
